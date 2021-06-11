4b2c6be42fe080bb12d0c6e2ed5caf24cf63982c
karthikmohan <karthik12mohan@gmail.com>
2021-06-11 20:25:08 +0530

    Initial commit

---
+++ app/src/main/java/com/example/gallery/MainActivity.java
@@ -0,0 +1,94 @@
+package com.example.gallery;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+import android.widget.Toast;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    List<Cell> allFilespath;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if(Build.VERSION.SDK_INT >=Build.VERSION_CODES.M && checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+            != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1000);
+        }
+        else{
+            showImages();
+        }
+    }
+    private void showImages(){
+        String path = Environment.getExternalStorageDirectory().getAbsolutePath()+"/Images/";
+        allFilespath= new ArrayList<>();
+        allFilespath=listAllFiles(path);
+
+        RecyclerView recyclerView= (RecyclerView) findViewById(R.id.gallery);
+        recyclerView.setHasFixedSize(true);
+
+        RecyclerView.LayoutManager layoutManager=new GridLayoutManager(getApplicationContext(),3);
+        recyclerView.setLayoutManager(layoutManager);
+
+        recyclerView.setItemViewCacheSize(20);
+        recyclerView.setDrawingCacheEnabled(true);
+        recyclerView.setDrawingCacheQuality(View.DRAWING_CACHE_QUALITY_HIGH);
+
+        ArrayList<Cell> cells= prepareData();
+        MyAdapter adapter= new MyAdapter(getApplicationContext(),cells);
+        recyclerView.setAdapter(adapter);
+    }
+    private ArrayList<Cell>prepareData(){
+        ArrayList<Cell>allImages = new ArrayList<>();
+        for(Cell c: allFilespath){
+            Cell cell = new Cell();
+            cell.setTitle(c.getTitle());
+            cell.setPath(c.getPath());
+            allImages.add(cell);
+        }
+        return allImages;
+
+    }
+
+    private List<Cell> listAllFiles(String pathName){
+        List<Cell>allFiles=new ArrayList<>();
+        File file= new File(pathName);
+        File[] files = file.listFiles();
+        if(files!=null){
+            for (File f : files){
+                Cell cell = new Cell();
+                cell.setTitle(f.getName());
+                cell.setPath(f.getAbsolutePath());
+                allFiles.add(cell);
+
+            }
+        }
+        return allFiles;
+
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode==1000){
+            if (grantResults[0]== PackageManager.PERMISSION_GRANTED){
+                showImages();
+            }else{
+                Toast.makeText(this, "Permission not granted", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        }
+    }
+}
